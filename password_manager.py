a3f56e7ba1ccd9a6d3d5f9d3c6c7bdbc02291fc6
snsakib <hello@snsakib.com>
2021-12-11 19:17:30 +0600

    password-manager

---
+++ password_manager.py
@@ -0,0 +1,45 @@
+from cryptography.fernet import Fernet
+
+'''
+def write_key():
+  key = Fernet.generate_key()
+  with open("key.key", "wb") as key_file:
+    key_file.write(key)
+
+write_key()
+'''
+
+def load_key():
+  file = open("key.key", "rb")
+  key = file.read()
+  file.close()
+  return key
+
+key = load_key()
+fer = Fernet(key)
+
+def view():
+  with open('passwords.txt', 'r') as f:
+    for line in f.readlines():
+      data = line.rstrip()
+      user, password = data.split("|")
+      print("User: ", user, ", Password: ", fer.decrypt(password.encode()).decode())
+
+def add():
+  name = input('Account Name: ')
+  pwd = input('Password: ')
+
+  with open("passwords.txt", 'a') as f:
+      f.write(name + "|" + str(fer.encrypt(pwd.encode()).decode()) + "\n")
+
+while True:
+  mode = input("Would you like to add a new password or view existing ones (view, add)? (Press 'q' to exit) ")
+  if mode == "q":
+    break
+  if mode == 'view':
+    view()
+  elif mode == 'add':
+    add()
+  else:
+    print('Invalid mode')
+    continue
